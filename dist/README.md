dabfebcffb7e3ec31ef7093b5411cb258bf27ff2
LNDavid <ldavid@rmacan.com>
2022-04-24 16:52:06 -0400

    update license functions to switch

--- dist/README.md
+++ dist/README.md
@@ -1,19 +1,18 @@
 
-        ===========
-        QUESTIONS??
-        ===========
+===========
+QUESTIONS??
+===========
 
-        If you have any questions, please see my contact details below:
+If you have any questions, please see my contact details below:
         
-        ## GitHub Address
+## GitHub Address
     
-        My GitHub address may be accessed here #[GitHub address](<a href=lnd4812>here</a>)
+My GitHub address may be accessed here #[GitHub address](<a href=ohpsf>here</a>)
         
-        ## GitHub Repository
+## GitHub Repository
     
-        Please click on the following link to access the repository for this program #[GitHub Repo Link git@github.com/lnd4812/readme-generator.git](https://github.com/lnd4812/readme-generator)
+Please click on the following link to access the repository for this program #[GitHub Repo Link oisf](https://github.com/lnd4812/readme-generator)
         
-        ## Contact information
+## Contact information
         
-        To contact me directly, please feel free to drop me an e-mail at the following address: #[email address]mailto:laureldavid64@gmail.com>laureldavid64@gmail.com</a>)
-   
+To contact me directly, please feel free to drop me an e-mail at the following address: [email address](<a hef="mailto:iof">laureldavid64@gmail.com</a>);
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,42 +1,68 @@
+// template that will create the README file
+
 // function to return a license badge based on which license is passed in; if no license, an empty string is returned
 function renderLicenseLink(license) {
     return ` 
-        if (${license} === 'MIT') {
-            licenseLink = [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)];
-        } else if 
-            (${license} === 'Apache') { 
-            licenseLink = ([![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)];
-        } else if
-            (${license} === 'BSD 2-clause') {
-            licenseLink = [![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)];
-        } else if
-            (${license} === 'BSD 3-clause') {
-            licenseLink = [![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)];
-        } else if    
-            (${license} = 'CC0') {
-            licenseLink = [![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)];
-        } else licenseLink = "";    
+    
+    switch ${license} {
+
+        case "MIT":
+        licenseLink = [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)];
+        break;
+
+        case "Apache":
+        licenseLink = ([![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)];
+        break;
+
+        case "BSD 2-clause":
+        licenseLink = [![License: BSD 2-clause](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)];
+        break;
+
+        case "BSD 3-clause":
+        licenseLink = [![License: BSD 3-clause](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)];
+        break;
+
+        case "CC0":
+        licenseLink = [![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)];
+        break;
+
+        default:
+        licenseLink = "";
+        break;
+    }
+    
     `;
 };
 
 // function that returns the license section of README; if there is no license, an empty string is returned
 function renderLicenseSection(license) {    
     return ` 
-        if (${license} === 'MIT') {
-                licenseSection = [MIT license link](https://choosealicense.com/licenses/mit/);
-        } else if 
-            (${license} === 'Apache') { 
-                licenseSection = [Apache license link](https://choosealicense.com/licenses/apache-2.0/);
-        } else if
-            (${license} === 'BSD 2-clause') {
-                licenseSection = [2-Clause BSD license link](https://opensource.org/licenses/BSD-2-Clause);
-        } else if
-            (${license} === 'BSD 3-clause') {
-                licenseSection = [3-Clause BSD license link](https://opensource.org/licenses/BSD-3-Clause);
-        } else if    
-            (${license} = 'CC0') {
-                licenseSection = [Creative Commons Zero](https://creativecommons.org/publicdomain/zero/1.0/);
-        } else licenseSection = "";    
+    switch ${license} {
+
+        case "MIT":
+        licenseSection = [MIT license link](https://choosealicense.com/licenses/mit/);
+        break;
+
+        case "Apache":
+        licenseSection = [Apache license link](https://choosealicense.com/licenses/apache-2.0/);
+        break;
+
+        case "BSD 2-clause":
+        licenseSection = [2-Clause BSD license link](https://opensource.org/licenses/BSD-2-Clause);
+        break;
+
+        case "BSD 3-clause":
+        licenseSection = [3-Clause BSD license link](https://opensource.org/licenses/BSD-3-Clause);
+        break;
+
+        case "CC0":
+        licenseSection = [Creative Commons Zero](https://creativecommons.org/publicdomain/zero/1.0/);
+        break;
+
+        default:
+        licenseSection = "";
+        break;
+    }
     `;
 };
 
@@ -44,71 +70,68 @@ function renderLicenseSection(license) {
 
 function generateMarkdown(data) {
   
-  return `
-     
-    # ${data.title}
+return `
+# ${data.title}
   
-    ## Description
+## Description
     
-    ${data.description}
+${data.description}
 
-    ## Table of Contents
+## Table of Contents
     
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [Credits](#credits)
-    * [License](#license)
+[Installation](https://github.com/lnd4812/readme-generator#installation)
+[Usage](https://github.com/lnd4812/readme-generator#usage)
+[Credits](https://github.com/lnd4812/readme-generator#credits)
+[License](https://github.com/lnd4812/readme-generator#license)
     
-   ## Installation
+## Installation
     
-   ${data.installation}
+${data.installation}
     
-   ##  Usage
+##  Usage
    
-   ${data.usage}
+${data.usage}
     
-   ## License
+## License
     
-   This repository includes [license](${renderLicenseLink}${renderLicenseSection}(data.license)}).
+This repository includes [license](${renderLicenseLink}${renderLicenseSection}(data.license)}).
 
-   ## Contributing
+## Contributing
    
-   ${data.contribute}
+${data.contribute}
     
-   ## Testing
+## Testing
    
-   ${data.tests}
-  `;
-};
+${data.tests}
+
+`;
+}
+
 function generateMarkdown(questions) {
     
-    return   `
-        ===========
-        QUESTIONS??
-        ===========
+return   `
+===========
+QUESTIONS??
+===========
 
-        If you have any questions, please see my contact details below:
+If you have any questions, please see my contact details below:
         
-        ## GitHub Address
+## GitHub Address
     
-        My GitHub address may be accessed here #[GitHub address](<a href=${questions.github}>here</a>)
+My GitHub address may be accessed here #[GitHub address](<a href=${questions.github}>here</a>)
         
-        ## GitHub Repository
+## GitHub Repository
     
-        Please click on the following link to access the repository for this program #[GitHub Repo Link ${questions.link}](https://github.com/lnd4812/readme-generator)
+Please click on the following link to access the repository for this program #[GitHub Repo Link ${questions.link}](https://github.com/lnd4812/readme-generator)
         
-        ## Contact information
+## Contact information
         
-        To contact me directly, please feel free to drop me an e-mail at the following address: #[email address]mailto:${questions.contact}>laureldavid64@gmail.com</a>)
-   `;
+To contact me directly, please feel free to drop me an e-mail at the following address: [email address](<a hef="mailto:${questions.contact}">laureldavid64@gmail.com</a>);
+`;
   
 };
       
 // access information produced from index.js file
-module.exports = generateMarkdown
-    // const {data, questions} = generateMarkdown;
-    // return`
-    //     ${generateDetails(data)}
-    //     ${generateQuestions(questions)}
-      
-    // `};
+module.exports = generateMarkdown;
+    
+
